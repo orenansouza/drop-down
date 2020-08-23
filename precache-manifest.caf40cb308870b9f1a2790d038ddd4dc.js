8480f45af56aec049b180252d0fcf6595497991e
orenansouza <renansouza.21.109@icloud.com>
2020-08-23 19:33:36 -0300

    Updates

---
+++ precache-manifest.caf40cb308870b9f1a2790d038ddd4dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cf004b5562a6ed983aed0751a87234e",
+    "url": "/drop-down/index.html"
+  },
+  {
+    "revision": "0574d58f8d0348424273",
+    "url": "/drop-down/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "2941bb081f110a2731ca",
+    "url": "/drop-down/static/js/2.eaa647c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drop-down/static/js/2.eaa647c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0574d58f8d0348424273",
+    "url": "/drop-down/static/js/main.0f05f5e8.chunk.js"
+  },
+  {
+    "revision": "d3ea7b7113c40583a588",
+    "url": "/drop-down/static/js/runtime-main.6bbc1668.js"
+  }
+]);
